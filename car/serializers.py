550eb5cea40dc61d19409827812e31fdf7fcda3e
Andrii Yermolenko <vakt159@gmail.com>
2025-12-24 18:40:46 +0200

    Flake8 fixed

--- car/serializers.py
+++ car/serializers.py
@@ -20,9 +20,11 @@ class CarSerializer(serializers.Serializer):
         instance.model = validated_data.get("model", instance.model)
         instance.horse_powers = validated_data.get("horse_powers",
                                                    instance.horse_powers)
-        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
-        instance.problem_description = validated_data.get("problem_description",
-                                                          instance.instance.is_broken)
+        instance.is_broken = validated_data.get("is_broken",
+                                                instance.is_broken)
+        instance.problem_description = validated_data.get(
+            "problem_description",
+            instance.instance.is_broken)
 
         instance.save()
         return instance
